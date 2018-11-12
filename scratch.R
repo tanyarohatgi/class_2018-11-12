addbcf9f7fc8cc51bf6730d0ddad56379367d216
Tanya Rohatgi <tanyarohatgi@Tanyas-MacBook-Pro-New.local>
2018-11-12 14:17:31 -0500

    Update.

--- scratch.R
+++ scratch.R
@@ -14,3 +14,13 @@ build_link_2 <- function(district, wave){
   paste("https://github.com/TheUpshot/2018-live-poll-results/tree/master/data/elections-poll-", district, 
         "-", wave, ".csv", sep = "")
 }
+
+download_link <- function(district, wave){
+  district <- str_to_lower(district)
+  district <- str_replace(district, "-", "")
+  x <- paste("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/elections-poll-", district, 
+        "-", wave, ".csv", sep = "")
+  y <- read_csv(x)
+  
+  invisible(y)
+}
